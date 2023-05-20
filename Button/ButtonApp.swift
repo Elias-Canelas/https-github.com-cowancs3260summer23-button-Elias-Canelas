e1760b01d0ba94ba737b786984c48846484d91fb
Elias-Canelas <eliasrojas3535@gmail.com>
2023-05-19 20:56:11 -0600

    Initial Commit

---
+++ Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Elias on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
